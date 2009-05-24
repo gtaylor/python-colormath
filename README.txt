87025b0a343d6e13dcd2297984e3bd3ff319d63c
Greg Taylor <gtaylor@duointeractive.com>
2009-05-24 22:20:32 +0000

    Re-organization, adding the license, and adding a setup.py distutils script.

---
+++ README.txt
@@ -0,0 +1,53 @@
+                            Python Color Math Module
+                                   (colormath)
+
+What is it?
+-----------
+This module implements a large number of different color operations such as
+color space conversions, Delta E, and density to spectral.
+
+Why?
+----
+Public documentation for color math is very limited, and there were no Python
+color math libraries with even a fraction of the common routines implemented.
+
+Requirements
+------------
+colormath requires NumPy, which you may get from:
+ 
+    http://numpy.scipy.org/
+
+Installation
+------------
+As root/admin on your machine:
+  python setup.py install
+  
+Documentation
+-------------
+As of right now, the only documentation are the examples in the examples
+directory. There's a whole lot going on right now, and not much time for
+cleanly documenting this stuff. However, the source code is very well
+commented.
+
+Support
+-------
+Head over to http://code.google.com/p/python-colormath/issues/list
+and submut an issue if you have any problems or questions.
+
+Legal Mumbo Jumbo
+-----------------
+Copyright (C) 2009 Gregory Taylor
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
---
+++ setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+"""
+ Color Math Module (colormath) 
+ Copyright (C) 2009 Gregory Taylor
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+from distutils.core import setup
+
+LONG_DESCRIPTION = \
+"""Implements a large number of different color operations such as
+color space conversions, Delta E, and density to spectral."""
+
+CLASSIFIERS = [
+                'Development Status :: 5 - Production/Stable',
+                'Intended Audience :: Developers',
+                'License :: OSI Approved :: GNU General Public License (GPL)',
+                'Natural Language :: English',
+                'Operating System :: OS Independent',
+                'Programming Language :: Python',
+                'Topic :: Scientific/Engineering :: Mathematics',
+                'Topic :: Software Development :: Libraries :: Python Modules' 
+              ]
+
+KEYWORDS = 'color math conversions'
+
+setup(name='colormath',
+      version='1.0.0',
+      description='Color math and conversion library.',
+      long_description = LONG_DESCRIPTION,
+      author='Gregory Taylor',
+      author_email='gtaylor@l11solutions.com',
+      url='http://code.google.com/p/python-colormath/',
+      download_url='http://code.google.com/p/python-colormath/',
+      packages=['colormath'],
+      platforms = 'Platform Independent',
+      license = 'GPLv3',
+      classifiers = CLASSIFIERS,
+      keywords = KEYWORDS,
+     )
