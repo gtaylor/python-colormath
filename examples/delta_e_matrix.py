25f29d5d5482b943af53e259393ab581c1a69ec7
eddie bell <eddie@lyst.com>
2014-01-14 16:15:40 +0000

    skeleton example

---
+++ examples/delta_e_matrix.py
@@ -0,0 +1,12 @@
+"""
+
+For a massive matrix of colors and color labels you can download
+the follow two files
+
+# http://lyst-classifiers.s3.amazonaws.com/color/lab-colors.pk
+# http://lyst-classifiers.s3.amazonaws.com/color/lab-matrix.pk
+
+lab-colors is a cPickled list of color names and lab-matrix is a
+cPickled (n,3) numpy array LAB values such that row q maps to
+index q in the lab color list
+"""
