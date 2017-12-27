671e96528c056f31aad6c81e0a1c0bd4fea270fd
Greg Taylor <greg.taylor@reddit.com>
2017-12-27 11:45:36 -0800

    Shift Python 3 supported versions up to 3.5/3.6

--- setup.py
+++ setup.py
@@ -16,8 +16,8 @@ CLASSIFIERS = [
     'Topic :: Scientific/Engineering :: Mathematics',
     'Topic :: Software Development :: Libraries :: Python Modules',
     'Programming Language :: Python :: 2.7',
-    'Programming Language :: Python :: 3.3',
-    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
 ]
 
 KEYWORDS = 'color math conversions'
